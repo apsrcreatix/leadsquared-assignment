46f4dbd6cc298b35558d5a399a69afcf8169bbc2
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 02:17:23 +0530

    Theme fixing in page loader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PageLoader = () => {
     return (
       <div className="d-flex justify-content-center align-items-center w-100vw h-100vh">
-        <div className="spinner-border" role="status">
+        <div className="spinner-border text-primary" role="status">
           <span className="sr-only">Loading...</span>
         </div>
       </div>
