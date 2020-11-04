a5df4745503edc01cc174a6295f8f8386b83346d
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 01:45:45 +0530

    Add Axios Instance Context

---
+++ src/contexts/AxiosContext.js
@@ -0,0 +1,39 @@
+import { createContext } from "react";
+import axios from "axios";
+
+const AxiosContext = createContext();
+const { Provider } = AxiosContext;
+
+const AxiosProvider = ({ children }) => {
+  const unauthenticatedAxios = axios.create({
+    baseURL: process.env.REACT_APP_API_ENDPOINT,
+  });
+
+  unauthenticatedAxios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  unauthenticatedAxios.interceptors.request.use(
+    (config) => {
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  return (
+    <Provider
+      value={{
+        unauthenticatedAxios,
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export { AxiosContext, AxiosProvider };
