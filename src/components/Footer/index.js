2411564cca387c29bb6d31a22d885a202f010823
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 22:02:49 +0530

    add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+const Footer = () => (
+  <footer
+    className="bg-primary text-white p-4"
+    style={{
+      margin: "0 -15px",
+    }}
+  >
+    <div className="row">
+      <div className="col-xl-6 col-12 text-xl-left text-center">
+        <div
+          className="h5 px-1 text-uppercase font-weight-bold mb-0"
+          style={{ letterSpacing: "2px" }}
+        >
+          Shop Express
+        </div>
+        <p className="lead px-1 mb-0" style={{ fontSize: "10px" }}>
+          Shop Limitless, Live Tensionless
+        </p>
+      </div>
+      <div className="col-xl-6 col-12">
+        <p className="text-xl-right text-center mb-0">
+          <small>
+            A product by{" "}
+            <a className="text-white" href="https://github.com/apsrcreatix">
+              apsrcreatix
+            </a>{" "}
+            © All Rights Reserved
+          </small>
+          <p className="lead px-1 mb-0" style={{ fontSize: "10px" }}>
+            Made with ☮️ in India
+          </p>
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
