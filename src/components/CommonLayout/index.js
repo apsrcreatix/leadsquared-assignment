718f1509faace105e5d58a59f551f90e1053ba7d
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 12:15:49 +0530

    add space in bottom

--- src/components/CommonLayout/index.js
+++ src/components/CommonLayout/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Header from '../Header';
 
 export default function CommonLayout({children}) {
-    return <div className="container-fluid">
+    return (
+      <div className="container-fluid">
         <Header />
         <div className="w-100 my-5 py-2" />
-        {children}
-    </div>
+       {children}
+        <div className="w-100 my-5 py-2" />
+      </div>
+    );
 }
