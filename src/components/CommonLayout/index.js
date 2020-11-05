91bc655c3266470c4a25ba034f74e1a49092023e
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 11:59:16 +0530

    add gap for sticky header

--- src/components/CommonLayout/index.js
+++ src/components/CommonLayout/index.js
@@ -4,6 +4,7 @@ import Header from '../Header';
 export default function CommonLayout({children}) {
     return <div className="container-fluid">
         <Header />
+        <div className="w-100 my-5 py-2" />
         {children}
     </div>
 }
