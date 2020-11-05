0d8d1d7b1325e6c7f08d12b95779abb5ca5f6313
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-11-05 22:03:07 +0530

    Include footer

--- src/components/CommonLayout/index.js
+++ src/components/CommonLayout/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from '../Header';
+import Footer from '../Footer';
+import PatternSlice from 'assets/images/dot-grid.png';
 
 export default function CommonLayout({children}) {
     return (
-      <div className="container-fluid">
+      <div
+            className="container-fluid"
+            style={{
+          background: `linear-gradient(to bottom, #ffffff, #ffffff90, #ffffff50), url(${PatternSlice})`,
+        }}
+      >
         <Header />
         <div className="w-100 my-5 py-2" />
-       {children}
+        {children}
         <div className="w-100 my-5 py-2" />
+        <Footer/>
       </div>
     );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as PageLoader } from "./PageLoader";
 export { default as Loader } from "./Loader";
 export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as CommonLayout } from "./CommonLayout";
